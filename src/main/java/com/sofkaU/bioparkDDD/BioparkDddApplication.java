3860774fe87e6b16241cb8986c1e860970db1bcd
jpabloallin <jpac.647@gmail.com>
2022-05-17 23:09:22 -0500

    Initial commit

---
+++ src/main/java/com/sofkaU/bioparkDDD/BioparkDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.bioparkDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioparkDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioparkDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/bioparkDDD/BioparkDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.bioparkDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioparkDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
