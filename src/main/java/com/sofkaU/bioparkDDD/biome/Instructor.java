2ab5cc226976a95ca1fa30d426f6ec3f7f0816a4
jpabloallin <jpac.647@gmail.com>
2022-05-18 22:33:23 -0500

    Created Instructor Entity with its behaviors

--- src/main/java/com/sofkaU/bioparkDDD/biome/Instructor.java
+++ src/main/java/com/sofkaU/bioparkDDD/biome/Instructor.java
@@ -1,4 +1,36 @@
 package com.sofkaU.bioparkDDD.biome;
 
-public class Instructor {
+import co.com.sofka.domain.generic.Entity;
+import com.sofkaU.bioparkDDD.biome.values.InstructorId;
+import com.sofkaU.bioparkDDD.biome.values.Name;
+import com.sofkaU.bioparkDDD.biome.values.YearsOfExperience;
+
+import java.util.Objects;
+
+public class Instructor extends Entity<InstructorId> {
+
+    private Name name;
+    private YearsOfExperience yearsOfExperience;
+
+    public Instructor(InstructorId entityId, Name name, YearsOfExperience yearsOfExperience) {
+        super(entityId);
+        this.name = name;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public void updateName(Name name) {
+        this.name = Objects.requireNonNull(name);
+    }
+
+    public void updateYearsOfExperience(YearsOfExperience yearsOfExperience) {
+        this.yearsOfExperience = Objects.requireNonNull(yearsOfExperience);
+    }
+
+    public Name name() {
+        return name;
+    }
+
+    public YearsOfExperience yearsOfExperience() {
+        return yearsOfExperience;
+    }
 }
