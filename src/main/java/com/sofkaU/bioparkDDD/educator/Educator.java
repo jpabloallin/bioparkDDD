b5e407b6700e6eaa75feead6506a1f2cd06463ab
jpabloallin <jpac.647@gmail.com>
2022-05-21 15:20:57 -0500

    Added behaviors with appended domain events on my aggregates.

--- src/main/java/com/sofkaU/bioparkDDD/educator/Educator.java
+++ src/main/java/com/sofkaU/bioparkDDD/educator/Educator.java
@@ -2,9 +2,13 @@ package com.sofkaU.bioparkDDD.educator;
 
 import co.com.sofka.domain.generic.AggregateEvent;
 import com.sofkaU.bioparkDDD.educator.events.EducatorCreated;
+import com.sofkaU.bioparkDDD.educator.values.BiologistId;
 import com.sofkaU.bioparkDDD.educator.values.EducatorId;
+import com.sofkaU.bioparkDDD.educator.values.GuideId;
+import com.sofkaU.bioparkDDD.educator.values.Name;
 import com.sofkaU.bioparkDDD.sharedvalueobjects.WorkArea;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Educator extends AggregateEvent<EducatorId> {
@@ -15,6 +19,25 @@ public class Educator extends AggregateEvent<EducatorId> {
     public Educator(EducatorId entityId, WorkArea workArea) {
         super(entityId);
         appendChange(new EducatorCreated(workArea)).apply();
+    }
+    public void updateWorkArea(WorkArea workArea) {
+        appendChange(new WorkAreaUpdated(workArea)).apply();
+    }
+    public void addBiologist(BiologistId entityId, Name name) {
+        appendChange(new BiologistAdded(entityId, name)).apply();
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+    }
+    public void updateBiologistName(BiologistId entityId, Name name) {
+        appendChange(new BiologistNameUpdated(entityId, name)).apply();
+    }
+    public void addGuide(GuideId entityId, Name name) {
+        appendChange(new GuideAdded(entityId, name)).apply();
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+    }
+    public void updateGuideName(BiologistId entityId, Name name) {
+        appendChange(new GuideNameUpdated(entityId, name)).apply();
     }
 
 }
--- src/main/java/com/sofkaU/bioparkDDD/staff/Staff.java
+++ src/main/java/com/sofkaU/bioparkDDD/staff/Staff.java
@@ -2,9 +2,14 @@ package com.sofkaU.bioparkDDD.staff;
 
 import co.com.sofka.domain.generic.AggregateEvent;
 import com.sofkaU.bioparkDDD.educator.events.EducatorCreated;
+import com.sofkaU.bioparkDDD.educator.values.BiologistId;
 import com.sofkaU.bioparkDDD.sharedvalueobjects.WorkArea;
+import com.sofkaU.bioparkDDD.staff.values.KeeperId;
+import com.sofkaU.bioparkDDD.staff.values.MaintenanceOperatorId;
+import com.sofkaU.bioparkDDD.staff.values.Name;
 import com.sofkaU.bioparkDDD.staff.values.StaffId;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Staff extends AggregateEvent<StaffId> {
@@ -15,5 +20,24 @@ public class Staff extends AggregateEvent<StaffId> {
     public Staff(StaffId entityId, WorkArea workArea) {
         super(entityId);
         appendChange(new EducatorCreated(workArea)).apply();
+    }
+    public void updateWorkArea(WorkArea workArea) {
+        appendChange(new WorkAreaUpdated(workArea)).apply();
+    }
+    public void addKeeper(KeeperId entityId, Name name) {
+        appendChange(new KeeperAdded(entityId, name)).apply();
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+    }
+    public void updateKeeperName(KeeperId entityId, Name name) {
+        appendChange(new KeeperNameUpdated(entityId, name)).apply();
+    }
+    public void addMaintenanceOperator(MaintenanceOperatorId entityId, Name name) {
+        appendChange(new MaintenanceOperatorAdded(entityId, name)).apply();
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+    }
+    public void updateMaintenanceOperatorName(MaintenanceOperatorId entityId, Name name) {
+        appendChange(new MaintenanceOperatorNameUpdated(entityId, name)).apply();
     }
 }
